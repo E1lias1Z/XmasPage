cf5f016d3094bce43d77700d94076d6f89d80cd7
E1lias1Z <ily.frolow2016@yandex.ru>
2022-02-24 16:04:55 +0400

    Added xmas page (other files)

---
+++ script/play.js
@@ -0,0 +1,15 @@
+var el = document.getElementById("let_it_snow");
+var playing = false;
+var player = new Audio("/sound/Let_It_Snow.mp3");
+
+player.preload = "auto";
+function playPause() {
+  if (playing) {
+    player.pause();
+  } else {
+    player.play();
+  }
+  playing = !playing;
+}
+
+el.addEventListener("click", playPause);
