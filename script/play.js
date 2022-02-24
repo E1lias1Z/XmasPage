59df2086b2d88e5a3a7d6b608fa2fa7cd4c2b9e8
E1lias1Z <ily.frolow2016@yandex.ru>
2022-02-24 16:31:25 +0400

    Updated script

--- script/play.js
+++ script/play.js
@@ -1,6 +1,8 @@
 var el = document.getElementById("let_it_snow");
 var playing = false;
-var player = new Audio("/sound/Let_It_Snow.mp3");
+var player = new Audio(
+  "https://e1lias1z.github.io/XmasPage/sound/Let_It_Snow.mp3"
+);
 
 player.preload = "auto";
 function playPause() {
